294ecc89c5062b5173ff2aac613b85350dbb316e
WANG Rui <wangrui@loongson.cn>
2025-02-10 17:13:15 +0800

    loongarch64: Add kvm-bindings
    Signed-off-by: WANG Rui <wangrui@loongson.cn>

--- kvm-bindings/src/lib.rs
+++ kvm-bindings/src/lib.rs
@@ -36,3 +36,8 @@ pub use self::arm64::*;
 mod riscv64;
 #[cfg(target_arch = "riscv64")]
 pub use self::riscv64::*;
+
+#[cfg(target_arch = "loongarch64")]
+mod loongarch64;
+#[cfg(target_arch = "loongarch64")]
+pub use self::loongarch64::*;
