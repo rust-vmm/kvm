8436ff1718b67d444825463f637df180767a4a67
BillXiang <xiangwencheng@lanxincomputing.com>
2025-09-09 19:14:08 +0800

    riscv64: Add unit test for SbiExt0_1ConsoleGetchar/Putchar
    running 1 test
    test ioctls::vcpu::tests::test_run_code ... ok
    
    Signed-off-by: BillXiang <xiangwencheng@lanxincomputing.com>

--- kvm-ioctls/src/ioctls/vcpu.rs
+++ kvm-ioctls/src/ioctls/vcpu.rs
@@ -2654,6 +2654,25 @@ mod tests {
             0x03, 0xa5, 0x0c, 0x00, // lw   a0, 0(s9);  test MMIO read
             0x93, 0x05, 0x70, 0x60, // li   a1, 0x0607;
             0x23, 0xa0, 0xbc, 0x00, // sw   a1, 0(s9);  test MMIO write
+            //sbi_console_getchar
+            0x01, 0x45, // li   a0, 0
+            0x81, 0x45, // li   a1, 0
+            0x01, 0x46, // li   a2, 0
+            0x81, 0x46, // li   a3, 0
+            0x01, 0x47, // li   a4, 0
+            0x81, 0x47, // li   a5, 0
+            0x01, 0x48, // li   a6, 0
+            0x89, 0x48, // li   a7, 2
+            0x73, 0x00, 0x00, 0x00, //ecall
+            //sbi_console_putchar
+            0x81, 0x45, // li   a1, 0
+            0x01, 0x46, // li   a2, 0
+            0x81, 0x46, // li   a3, 0
+            0x01, 0x47, // li   a4, 0
+            0x81, 0x47, // li   a5, 0
+            0x01, 0x48, // li   a6, 0
+            0x85, 0x48, // li   a7, 1
+            0x73, 0x00, 0x00, 0x00, //ecall
             0x6f, 0x00, 0x00, 0x00, // j .; shouldn't get here, but if so loop forever
         ];
 
@@ -2725,6 +2744,12 @@ mod tests {
                         .map(|page| page.count_ones())
                         .sum();
                     assert_eq!(dirty_pages, 1);
+                }
+                VcpuExit::SbiExt0_1ConsoleGetchar(data) => {
+                    data[0] = 0x2a;
+                }
+                VcpuExit::SbiExt0_1ConsolePutchar(data) => {
+                    assert_eq!(data, 0x2a);
                     break;
                 }
                 r => panic!("unexpected exit reason: {:?}", r),
